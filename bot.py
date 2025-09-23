c684a0164a3d9186ea06e8de1cad52047500cbe5
vaarday-a11y <vaarday@gmail.com>
2025-09-23 20:22:03 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,6 @@ def button_handler(update, context):
     # eski fayl bo‘lsa ham yangisini ustiga yozib yuboradi
     "overwrites": True
 }
-}
     else:  # audio
         ydl_opts = {
             "format": "bestaudio/best",
